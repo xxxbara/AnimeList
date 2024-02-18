0f8f5bf17aa11efc9425af0bd6bfc4cd18f4fa6f
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-02-18 20:19:38 +0700

    fix: validation pagination

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -6,27 +6,25 @@ const Pagination = ({ page, lastPage, setPage }) => {
     });
   };
   const handleNextPage = () => {
-    if (page < lastPage) {
-      setPage((prevState) => prevState + 1);
-    }
+    setPage((prevState) => prevState + 1);
     scrollTop();
   };
 
   const handlePrevPage = () => {
-    if (page > 1) {
-      setPage((prevState) => prevState - 1);
-    }
+    setPage((prevState) => prevState - 1);
     scrollTop();
   };
 
   return (
     <div className="flex justify-center items-center py-4 px-2 gap-4 text-color-primary text-2xl">
-      <button
-        onClick={handlePrevPage}
-        className="transition-all hover:text-color-accent"
-      >
-        Prev
-      </button>
+      {page <= 1 ? null : (
+        <button
+          onClick={handlePrevPage}
+          className="transition-all hover:text-color-accent"
+        >
+          Prev
+        </button>
+      )}
       <p>
         {page} of {lastPage}
       </p>
