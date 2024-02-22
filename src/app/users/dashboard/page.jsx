379ecece7ff47b8fc7c67235c7510f3b3c184fae
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-02-22 21:23:22 +0700

    add: middleware

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,15 +1,17 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
   const user = await authUserSession();
+
   return (
     <div className="text-color-primary text-2xl">
       <h3>DASHBOARD</h3>
-      <h5>Welcome, {user.name}</h5>
+      <h5>Welcome, {user?.name}</h5>
       <div>
         <Image
-          src={user.image}
+          src={user?.image}
           alt="profile picture"
           width={250}
           height={250}
